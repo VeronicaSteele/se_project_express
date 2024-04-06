950ebcee6ba9d9b6bcb836bf6dc4ab00501dd3ad
Veronica Steele <vmariesteele@gmail.com>
2024-04-06 14:56:36 -0500

    added controllers/users

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-const http = require('http');
-const server = http.createServer();
-server.listen(3000);
+const express = require('express');
+const mongoose = require ('mongoose');
+const mainRouter = require ("./routes/index");
+
+const app = express();
 const { PORT = 3001 } = process.env;
+
+mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db')
+.then (() =>{
+  console.log("connected to DB");
+})
+.catch (console.error);
+
+app.use(express.json());
+app.use('/', mainRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
