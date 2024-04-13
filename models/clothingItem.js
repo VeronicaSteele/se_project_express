ce5b0aa75cfe4533e665411b76723c093345cdee
Veronica Steele <vmariesteele@gmail.com>
2024-04-12 20:09:11 -0500

    updated routes/clothingITems

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const clothingItemSchema = new mongoose.Schema({
 name:{
@@ -15,6 +16,12 @@ weather:{
 imageURL:{
   type: String,
   required: true,
+  validate: {
+    validator(value) {
+      return validator.isURL(value);
+    },
+    message: 'You must enter a valid URL',
+  }
 },
 owner:{
   required: true,
